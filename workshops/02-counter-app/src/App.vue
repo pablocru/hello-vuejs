cb16c137abb379b627e0585ac780c67fd23e47b8
pablocru <pablocru@outlook.com>
2024-12-27 17:20:38 +0100

    feat(counter-app): Initialize counter app with Vue.js, TypeScript, and Vite configuration

---
+++ workshops/02-counter-app/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const title = "Hello, Vue.js";
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
---
+++ workshops/02-counter-app/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@hello-vuejs/styles";
+
+createApp(App).mount("#app");
