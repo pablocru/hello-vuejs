76796973be6315b597a14a0ec37d478fbd7ed876
pablocru <pablocru@outlook.com>
2024-12-27 17:22:58 +0100

    feat(counter-app): Add CounterComponent instances to App.vue with initial values

--- workshops/02-counter-app/src/App.vue
+++ workshops/02-counter-app/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import CounterComponent from "./components/CounterComponent.vue";
+
 const title = "Hello, Vue.js";
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+
+  <CounterComponent :initialValue="5" />
+  <CounterComponent :initialValue="10" />
 </template>
